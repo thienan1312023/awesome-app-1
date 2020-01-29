9b870d86fb3f3de1be8fb7e12c0b20a0cddbdc36
thienan <thienan2195@gmail.com>
2020-01-29 20:28:23 +0700

    add get POI

--- constants/key.js
+++ constants/key.js
@@ -1,7 +1,5 @@
-const API_CAGE_DATA = '16504694869646d7b0ddda342abf5732';
-const GeoCoding_URL = 'https://api.opencagedata.com/geocode/v1/json?q=';
-
-export {
-    API_CAGE_DATA,
-    GeoCoding_URL
-}
+const API_CAGE_DATA = "16504694869646d7b0ddda342abf5732";
+const GeoCoding_URL = "https://api.opencagedata.com/geocode/v1/json?q=";
+const POI_Detail_URL =
+  "https://maps.googleapis.com/maps/api/place/details/json?place_id=place_id_param&fields=formatted_address,name,rating,formatted_phone_number&key=AIzaSyDND5Y4CweE_-E6qfyLLxI0W6_oFcx_-4w";
+export { API_CAGE_DATA, GeoCoding_URL, POI_Detail_URL };
--- screens/create.js
+++ screens/create.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet, ActivityIndicator } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  Button
+} from "react-native";
 import MapView from "react-native-maps";
 import * as Location from "expo-location";
 import * as Permissions from "expo-permissions";
 import Constants from "expo-constants";
-import Indicator from "../components/AcitivityIndicator";
-import { GeoCoding_URL, API_CAGE_DATA } from "../constants/key";
+import Modal from "react-native-modal";
+import { GeoCoding_URL, API_CAGE_DATA, POI_Detail_URL } from "../constants/key";
+
 export default class Create extends Component {
   state = {
     mapRegion: null,
     hasLocationPermissions: false,
     locationResult: null,
-    isLoading: false
+    isLoading: false,
+    isModalVisible: false,
+    POIDetail: {
+      name: "",
+      formatted_address: "",
+      rating: 0,
+      formatted_phone_number: ""
+    }
   };
 
   componentDidMount() {
@@ -20,6 +34,10 @@ export default class Create extends Component {
 
   handleMapRegionChange = mapRegion => {
     this.setState({ mapRegion });
+  };
+
+  toggleModal = () => {
+    this.setState({ isModalVisible: !this.state.isModalVisible });
   };
 
   async getLocationAsync() {
@@ -46,27 +64,53 @@ export default class Create extends Component {
     });
   }
 
-  handlePressOnMap = event => {
-    this.setState({ isLoading: true });
-    const queryURL = `${GeoCoding_URL +
-      event.nativeEvent.coordinate.latitude}+${
-      event.nativeEvent.coordinate.longitude
-    }&key=${API_CAGE_DATA}`;
-    // console.log(queryURL);
+  handleOnPoiClick = event => {
+    const { placeId } = event.nativeEvent;
+    const queryURL = POI_Detail_URL.replace("place_id_param", placeId);
 
     return fetch(queryURL)
       .then(response => response.json())
       .then(responseJson => {
-        this.setState({ isLoading: true });
-        alert(JSON.stringify(responseJson));
+        this.setState({
+          isLoading: false,
+          isModalVisible: true,
+          POIDetail: {
+            ...responseJson.result
+          }
+        });
       })
       .catch(error => {
-        this.setState({ isLoading: true });
-        // console.error(error);
+        this.setState({ isLoading: false });
+        console.error(error);
       });
   };
+  // handleTapOnMap = () => {
+  //   this.setState({ isLoading: true });
+  //   const { latitude, longitude } = this.state.mapRegion;
+  //   console.log(latitude + " + " + longitude);
+  //   const queryURL = `${GeoCoding_URL +
+  //     latitude}+${longitude}&key=${API_CAGE_DATA}`;
+
+  //   return fetch(queryURL)
+  //     .then(response => response.json())
+  //     .then(responseJson => {
+  //       this.setState({ isLoading: false, isModalVisible: true });
+  //     })
+  //     .catch(error => {
+  //       this.setState({ isLoading: false });
+  //       console.error(error);
+  //     });
+  // };
   render() {
-    const { isLoading } = this.state;
+    const {
+      isLoading,
+      POIDetail: {
+        formatted_address: FormattedAddress,
+        formatted_phone_number: FormattedPhoneNumber,
+        name: Name,
+        rating: Rating
+      }
+    } = this.state;
 
     return (
       <View style={styles.container}>
@@ -89,12 +133,23 @@ export default class Create extends Component {
               style={{ alignSelf: "stretch", height: 400 }}
               region={this.state.mapRegion}
               onRegionChangeComplete={this.handleMapRegionChange}
-              onPress={event => this.handlePressOnMap(event)}
+              // onPress={this.handleTapOnMap}
+              onPoiClick={event => this.handleOnPoiClick(event)}
             />
           )}
 
           <Text>Location: {this.state.locationResult}</Text>
         </View>
+
+        <Modal isVisible={this.state.isModalVisible}>
+          <View style={styles.modal}>
+            <Text>{Name}</Text>
+            <Text>{FormattedAddress}</Text>
+            <Text>{FormattedPhoneNumber}</Text>
+            <Text>{Rating}</Text>
+            <Button title="Hide modal" onPress={this.toggleModal} />
+          </View>
+        </Modal>
       </View>
     );
   }
@@ -124,5 +179,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#F5FCFF88"
+  },
+  modal: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+    justifyContent: "center",
+    height: 50,
+    padding: 5
   }
 });
